e2f4e88a26b162eda7bf46d1f5abb813f7db3d4f
Jake Marsh <jakemmarsh@gmail.com>
2015-08-20 10:20:27 -0700

    Fix comment, add sandbox for tests

--- __tests__/helper.js
+++ __tests__/helper.js
@@ -1,17 +1,23 @@
 'use strict';
 
 before(function(done) {
-
-  // Ensure that 'should' library methods will be
-  // available to all tests
+  // Ensure that 'should' and 'sinon' library methods
+  // will be available to all tests
   global.should = require('should');
   global.sinon = require('sinon');
 
   // Start and configure the server
   require('../server');
 
-  // Wait half a second before calling "done" to ensure
+  // Wait one second before calling "done" to ensure
   // that server is up and running
-  setTimeout(done, 500);
+  setTimeout(done, 1000);
+});
 
+beforeEach(function() {
+  global.sandbox = sinon.sandbox.create();
+});
+
+afterEach(function() {
+  global.sandbox.restore();
 });
